2ee1b1de1bcfd71a8a93f138a5ca82be3963ed7f
Zahra <zahratussita421@gmail.com>
2023-10-18 12:51:47 +0700

    tugas pertama-zahra tussita-XI RPL 1

---
+++ app/src/main/java/com/example/latihan1/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.latihan1
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
